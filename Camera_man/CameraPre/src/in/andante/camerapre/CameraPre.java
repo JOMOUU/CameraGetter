b68bd85ecb7f39bfeaafad833a214abcd72675a3
s151009 <Red_Kerry@HO-rMBP13.local>
2014-10-23 03:19:02 +0900

    カメラ機能
    Camera_manというフォルダにもろもろ入ってます

---
+++ Camera_man/CameraPre/src/in/andante/camerapre/CameraPre.java
@@ -0,0 +1,28 @@
+package in.andante.camerapre;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+// メインアクティビティ
+public class CameraPre extends Activity {
+	/** Called when the activity is first created. */	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		CameraView view = new CameraView(this);
+		setContentView(view);
+		//setContentView(R.layout.main);
+	}
+
+	protected void onResume(){
+		super.onResume();
+	}
+ 
+	protected void onStop(){
+		super.onStop();
+	}
+ 
+	public void onDestroy(){
+		super.onDestroy();
+	}
+}
