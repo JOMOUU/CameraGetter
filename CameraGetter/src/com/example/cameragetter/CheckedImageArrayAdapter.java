14c23ad83ccc127061e8562c9fad6b09374f43e6
kerotang <kerotang@gmail.com>
2014-10-22 21:11:59 +0900

    色�

---
+++ CameraGetter/src/com/example/cameragetter/CheckedImageArrayAdapter.java
@@ -0,0 +1,162 @@
+package com.example.cameragetter;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;  
+import android.graphics.Color;
+import android.view.LayoutInflater;  
+import android.view.View;  
+import android.view.ViewGroup;  
+import android.widget.ArrayAdapter;  
+import android.widget.CheckBox;  
+import android.widget.CompoundButton;  
+import android.widget.ImageView;  
+import android.widget.CompoundButton.OnCheckedChangeListener;  
+  
+  
+public class CheckedImageArrayAdapter extends ArrayAdapter<CheckedImage> {  
+    private static class ViewHolder {  
+        int position;  
+        ImageView imageview = null;  
+        CheckBox checkbox = null;  
+    }  
+  
+    private final static int LAYOUT_ID = R.layout.list_item;  
+  
+  
+    private Context mContext;  
+    private LayoutInflater mInflater;     
+  
+  
+    public CheckedImageArrayAdapter(Context context, List<CheckedImage> objects) {  
+        super(context, LAYOUT_ID, objects);  
+        mContext = context;  
+        mInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);   
+    }  
+  
+    @Override  
+    public View getView(int position, View convertView, ViewGroup parent) {  
+        final ViewHolder holder;
+        if (convertView == null) {  
+            convertView = mInflater.inflate(LAYOUT_ID, null);  
+            holder = new ViewHolder();  
+            holder.imageview = (ImageView) convertView.findViewById(R.id.image);  
+            holder.checkbox = (CheckBox) convertView.findViewById(R.id.check);  
+            holder.checkbox.setOnCheckedChangeListener(CheckBox1_OnCheckedChangeListener);  
+            convertView.setTag(holder);  
+        } else {  
+            holder = (ViewHolder)convertView.getTag();  
+        }  
+  
+        convertView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO 自動生成されたメソッド・スタブ
+				if(holder.checkbox.isChecked()){
+					holder.checkbox.setChecked(false);
+				} else {
+					holder.checkbox.setChecked(true);
+				}
+			}
+		});
+        CheckedImage item = getItem(position);  
+        holder.position = position;  
+        holder.imageview.setImageBitmap(item.getBitmap(mContext));  
+        holder.checkbox.setChecked(item.getChecked());  
+  
+        return convertView;  
+    }  
+  
+    public List<CheckedImage> getCheckedItem(){  
+        List<CheckedImage> lstItem = new ArrayList<CheckedImage>();  
+        for ( int i = 0; i < getCount(); i++) {  
+            if (getItem(i).getChecked()){  
+                lstItem.add(getItem(i));  
+            }  
+        }  
+        return lstItem;  
+    }  
+    
+    public List<Long> getCheckedItemId(){
+        List<Long> lstItemId = new ArrayList<Long>();  
+        for ( int i = 0; i < getCount(); i++) {  
+            if (getItem(i).getChecked()){  
+                lstItemId.add(getItem(i).getBitmapId());  
+            }  
+        }                 	
+    	return lstItemId;
+    }
+    
+  
+    private OnCheckedChangeListener CheckBox1_OnCheckedChangeListener = new OnCheckedChangeListener(){  
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {  
+            View view = (View)buttonView.getParent();  
+            ViewHolder holder = (ViewHolder)view.getTag();  
+            CheckedImage item = CheckedImageArrayAdapter.this.getItem(holder.position);  
+            item.setChecked(isChecked);          
+        }};  
+}  
+
+
+/*import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.view.View.OnTouchListener;
+ 
+public class BitmapAdapter extends ArrayAdapter<Bitmap> {
+ 
+    private int resourceId;
+ 
+    public BitmapAdapter(Context context, int resource, List<Bitmap> objects) {
+        super(context, resource, objects);
+        resourceId = resource;
+    }
+    
+    private static class ViewHolder {
+        public ImageView imageView;
+        public CheckBox checkBox;
+     }
+    
+ 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(resourceId, null);
+            holder = new ViewHolder();
+            holder.imageView = (ImageView)convertView.findViewById(R.id.image);
+            holder.checkBox = (CheckBox)convertView.findViewById(R.id.check);
+            convertView.setTag(holder);
+        } else {
+        	holder = (ViewHolder)convertView.getTag();
+        }
+ 
+        
+        holder.imageView.setImageBitmap(getItem(position));
+        convertView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO 自動生成されたメソッド・スタブ
+				if(holder.checkBox.isChecked()){
+					holder.checkBox.setChecked(false);
+				} else {
+					holder.checkBox.setChecked(true);
+				}
+			}
+		});
+         
+        return convertView;
+    }
+ 
+}*/
--- CameraGetter/src/com/example/cameragetter/MainActivity.java
+++ CameraGetter/src/com/example/cameragetter/MainActivity.java
@@ -1,47 +1,136 @@
 package com.example.cameragetter;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.GridView;
- 
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
+
+
 public class MainActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
- 
-        ArrayList<Bitmap> list = load();
-        BitmapAdapter adapter = new BitmapAdapter(
-                getApplicationContext(), R.layout.list_item,
-                list);
- 
-        GridView gridView = (GridView) findViewById(R.id.gridView);
-        gridView.setAdapter(adapter);
- 
- 
-    }
- 
-    private ArrayList<Bitmap> load() {
-        ArrayList<Bitmap> list = new ArrayList<Bitmap>();
-        ContentResolver cr = getContentResolver();
-        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-        ContentResolver resolver = getContentResolver();
-        //Cursor c = managedQuery(uri, null, null, null, null);
-        Cursor c = resolver.query(uri, null, null, null, null);
-        c.moveToFirst();
-        for (int i = 0; i < c.getCount(); i++) {
-            long id = c.getLong(c.getColumnIndexOrThrow("_id"));
-            Bitmap bmp = MediaStore.Images.Thumbnails.getThumbnail(cr, id, MediaStore.Images.Thumbnails.MINI_KIND, null);
-            list.add(bmp);
-            c.moveToNext();
+    private GridView mGridView = null;  
+    private Button mButton = null;  
+    private PopupWindow mPopupWindow;
+    private Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI; //SDカード (とは限らない。プライマリな領域を指す)
+
+    @Override  
+    public void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.activity_main);  
+  
+        mGridView = (GridView)findViewById(R.id.gridView);  
+        mButton = (Button)findViewById(R.id.button1);  
+        mButton.setOnClickListener(ButtonSelect_OnClickListener);  
+  
+        //画像データのIDを取得          
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);          
+        ArrayList<CheckedImage> lstItem = new ArrayList<CheckedImage>();  
+        cursor.moveToFirst();     
+        for (int i = 0; i < cursor.getCount(); i++){  
+            long id = cursor.getLong(cursor.getColumnIndexOrThrow("_id"));  
+            lstItem.add(new CheckedImage(true,id));  
+            cursor.moveToNext();  
+        }  
+  
+        //グリッド用のアダプターを作成  
+        CheckedImageArrayAdapter adapter = new CheckedImageArrayAdapter(getApplicationContext(),lstItem);  
+        //グリッドにアダプターをセット  
+        mGridView.setAdapter(adapter);  
+  
+    }  
+    
+    
+    /*
+     * 完了ボタンを押したときの処理
+     * */
+    private OnClickListener ButtonSelect_OnClickListener = new OnClickListener(){  
+    	@Override
+        public void onClick(View view) {  
+    
+    
+    		mPopupWindow = new PopupWindow(MainActivity.this);
+            // レイアウト設定
+            View popupView = getLayoutInflater().inflate(R.layout.popup, null);
+            
+            //閉じるを押したときの処理
+            popupView.findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mPopupWindow.isShowing()) {
+                        mPopupWindow.dismiss();
+                    }
+                }
+            });
+            //はいを押したときの処理
+            popupView.findViewById(R.id.ok_button).setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO 自動生成されたメソッド・スタブ
+					
+			        CheckedImageArrayAdapter adapter = (CheckedImageArrayAdapter)mGridView.getAdapter();  
+			        List<CheckedImage> lstCheckedItem = adapter.getCheckedItem();
+
+
+			        /////////////////////////////////////
+			        ///////
+			        ///////okuda
+			        ////////
+			        ////////////////////////////////////
+			        
+			        
+			        
+			        
+					//作業領域の画像全部消す
+					//getContentResolver().delete(uri, null, null);
+			        
+				}
+			});
+            
+            mPopupWindow.setContentView(popupView);
+
+
+            // タップ時に他のViewでキャッチされないための設定
+            mPopupWindow.setOutsideTouchable(true);
+            mPopupWindow.setFocusable(true);
+
+            // 表示サイズの設定 今回は幅300dp
+            float width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics());
+            mPopupWindow.setWindowLayoutMode((int) width, WindowManager.LayoutParams.WRAP_CONTENT);
+            mPopupWindow.setWidth((int) width);
+            mPopupWindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+
+            // 画面中央に表示
+            mPopupWindow.showAtLocation(findViewById(R.id.gridView), Gravity.CENTER, 0, 0);
         }
-        return list;
-    }
-}
+
+
+    };  	
+	
+    @Override
+    protected void onDestroy() {
+        if (mPopupWindow != null && mPopupWindow.isShowing()) {
+            mPopupWindow.dismiss();
+        }
+        super.onDestroy();
+    	
+    }  
+
+
+    
+}   
+
